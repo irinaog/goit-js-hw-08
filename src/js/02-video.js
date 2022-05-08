aa50168b35e92ace529f41ac4b20cfc397c799bf
irinaog <goncharirina08@gmail.com>
2022-05-08 15:25:08 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+const vimeo = require('@vimeo/player');
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+onContinuePlay();
+
+// player.on('timeupdate', function (data) {
+//     const time = JSON.stringify(data.seconds);
+//     localStorage.setItem(STORAGE_KEY, time);
+// });
+player.on('timeupdate', throttle(onRemindTimePlay, 1000));
+
+function onRemindTimePlay (data) {
+    const time = JSON.stringify(data.seconds);
+    localStorage.setItem(STORAGE_KEY, time);
+}
+ 
+function onContinuePlay() {
+    const currentTime = localStorage.getItem(STORAGE_KEY);
+    if (currentTime) {
+        player.setCurrentTime(currentTime);
+    }
+};
 
